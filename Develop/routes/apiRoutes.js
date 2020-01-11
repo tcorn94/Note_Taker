cb38fa56d11ced64b289f7b76e6cdc856a02e056
Princess Itaya <taycornelius@att.net>
2020-01-11 14:20:53 -0500

    updates to apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -11,6 +11,22 @@ router.get("/notes", function(req, res){
 
 });
 
+router.post("/notes", (req, res) => {
+    dbManager
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+
+} );
+
+router.delete("/notes/:id", function(req, res){
+    dbManager
+    .removeNote(req.params.id)
+    .then(() => res.json({ok:true}))
+    .catch(err => res.status(500).json(err));
+
+});
+
 
 
 
