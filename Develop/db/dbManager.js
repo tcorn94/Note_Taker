432fd66e44e62c7a85f8eb0ec41aaaa82087d8ca
Princess Itaya <taycornelius@att.net>
2020-01-11 14:10:36 -0500

    writing dbmanager file

---
+++ Develop/db/dbManager.js
@@ -0,0 +1,36 @@
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class dbManager {
+constructor(){
+    this.lastId = 0;
+}
+
+read(){
+
+}
+
+write(note){
+
+}
+
+
+getNotes(){
+
+}
+
+addNote(note){
+
+}
+
+
+
+removeNote(id){
+
+}
+
+
+
+
+
+}
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,52 +1,73 @@
-app.get("/api/notes", function(req, res) {
-    //readfile
-    var data;
-    fs.readFile('../db/db.json', 'utf8', function read(err, notes)
-    {
-        if(err)
-        throw err;
+const router = require("express").Router();
+const dbManager = require("../db/dbManager");
+
+
+router.get("/notes", function(req, res){
+    dbManager
+    .getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+
+
+});
+
+
+
+
+
+
+
+// app.get("/api/notes", function(req, res) {
+//     //readfile
+//     var data;
+//     fs.readFile('../db/db.json', 'utf8', function read(err, notes)
+//     {
+//         if(err)
+//         throw err;
         
-        data = JSON.parse(JSON.stringify(notes));
-        console.log(data);
-        res.send(data);
+//         data = JSON.parse(JSON.stringify(notes));
+//         console.log(data);
+//         res.send(data);
     
-    }  );
+//     }  );
     
-    })
+//     })
     
-    app.post("/api/notes", function(req, res) {
-    //read, delete, add, rewrite
-    var dt = new Date().getTime();
+//     app.post("/api/notes", function(req, res) {
+//     //read, delete, add, rewrite
+//     var dt = new Date().getTime();
     
-    gId = "";
+//     gId = "";
     
-    for(i=0; i<16; i++){
-        var r = (dt + Math.random()*16)%16 | 0;
-        gId += r;
-    }
+//     for(i=0; i<16; i++){
+//         var r = (dt + Math.random()*16)%16 | 0;
+//         gId += r;
+//     }
     
-    req.body.id = gId;
-    //console.log(req);
-    console.log(req.body);
-    var x = JSON.stringify(req.body);
+//     req.body.id = gId;
+//     //console.log(req);
+//     console.log(req.body);
+//     var x = JSON.stringify(req.body);
     
-    fs.appendFile('../db/db.json',", \n"+ x, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-    //add property unique id
+//     fs.appendFile('../db/db.json',", \n"+ x, function (err) {
+//         if (err) throw err;
+//         console.log('Saved!');
+//       });
+//     //add property unique id
     
-    })
+//     })
     
-    app.delete("/api/notes/:id", () => {
-        var uniqueID = req.params.id;
+//     app.delete("/api/notes/:id", () => {
+//         var uniqueID = req.params.id;
     
-        console.log(uniqueID);
+//         console.log(uniqueID);
         
         
-        //how did they grab id from front end
-        //read, rewrite file
-        res.json(deleteNote);
-        res.json(getAndRenderNotes);
+//         //how did they grab id from front end
+//         //read, rewrite file
+//         res.json(deleteNote);
+//         res.json(getAndRenderNotes);
     
-    })
+//     })
+
+    module.exports = router;
